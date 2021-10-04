536b721e164d7b764c3c909b1467ba169b8c735f
Alice Rum <alice.rum@redhat.com>
2021-10-04 15:51:32 +0200

    neovim configs install script

---
+++ neovim/install.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# directory in which this script is located
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+mkdir "$HOME/.config/nvim"
+pushd "$HOME/.config/nvim" >/dev/null || exit 1
+
+# create symlinks to neovim config fileses
+for i in $(find "$SCRIPT_DIR" -name "*.vim" -type f); do
+	ln -s "$i" .
+done
+
+#symlink to coc settings
+ln -s "$SCRIPT_DIR/coc-settings.json" .
+popd >/dev/null || exit 1
+
+# install plug.vim
+sh -c "curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"

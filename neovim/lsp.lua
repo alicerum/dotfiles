e5f95f9cdb3121b8c5b5815ed1692e14eb830c82
Alice Rum <wyvie@wyvie.org>
2022-06-04 09:44:04 +0200

    enable pyright, disable phpdoc ts plugin

--- neovim/lsp.lua
+++ neovim/lsp.lua
@@ -35,7 +35,7 @@ capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
-local servers = { 'hls', 'gopls', 'rls' }
+local servers = { 'hls', 'gopls', 'rls', 'pyright' }
 for _, lsp in pairs(servers) do
   require('lspconfig')[lsp].setup {
     capabilities = capabilities,
--- neovim/treesitter.lua
+++ neovim/treesitter.lua
@@ -6,8 +6,9 @@ require'nvim-treesitter.configs'.setup {
     -- Using this option may slow down your editor, and you may see some duplicate highlights.
     -- Instead of true it can also be a list of languages
     additional_vim_regex_highlighting = false,
+    disable = { "phpdoc", "tree-sitter-phpdoc" },
   },
 
   -- One of "all", "maintained" (parsers with maintainers), or a list of languages
-  ensure_installed = "all",
+  -- ensure_installed = "maintained",
 }
